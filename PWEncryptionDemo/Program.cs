86bfc43895784f7dca4cb5677619c2bd17fc4974
biel <git.gi7ba@slmail.me>
2022-11-18 20:03:01 -0700

    Encryption Demo initial commit

--- PWEncryptionDemo/Program.cs
+++ PWEncryptionDemo/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello");
+// Generate a key k1 with password pwd1 and salt salt1.
+// Generate a key k2 with password pwd1 and salt salt1.
+// Encrypt data1 with key k1 using symmetric encryption, creating edata1.
+// Decrypt edata1 with key k2 using symmetric decryption, creating data2.
+// data2 should equal data1.
